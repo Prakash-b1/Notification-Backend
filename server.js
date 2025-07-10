ba3bedc4498818d358fb10193faab79bf6a733b9
Prakash-b1 <pinnub23@gmail.com>
2025-07-10 11:56:37 +0530

    changes

--- server.js
+++ server.js
@@ -75,6 +75,9 @@ setInterval(deleteNormalNotifications, 86400000);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'build')));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
